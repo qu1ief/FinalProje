d8f9b8e2b4eebc54cc6e6e9e73ab2229fcd5fea9
Sabir164 <sabirkg@code.edu.az>
2024-04-20 14:27:20 +0400

    end commit

--- js/HomeScript.js
+++ js/HomeScript.js
@@ -1,38 +1,38 @@
 const ProductData = [
     {
         "id": 1,
-        "name": "Watch1",
+        "name": "Phone",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-3.png&w=640&q=75",
         "price": 100,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 2,
-        "name": "Watch2",
+        "name": "Shoes",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-1.png&w=384&q=75",
         "price": 120,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 3,
-        "name": "Watch3",
+        "name": "Watch",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-2.png&w=640&q=75",
         "price": 110,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 4,
         "name": "Watch4",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-4.png&w=640&q=75",
         "price": 140,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 5,
-        "name": "Watch",
+        "name": "Watch5",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-4.png&w=640&q=75",
         "price": 160,
-        "disCount":200
+        "disCount": 200
     }
 ]
 
@@ -43,8 +43,8 @@ GetBasket2()
 
 
 async function GetHomeProducts() {
-    const HomeSWiperContiner=document.getElementById("SecondSwiperHome")
-// const response = await axios('yourendpoint')
+    const HomeSWiperContiner = document.getElementById("SecondSwiperHome")
+    // const response = await axios('yourendpoint')
 
     // Get product data in place
     ProductData.forEach(element => {
@@ -176,6 +176,38 @@ function GetBasket() {
 
 
 
+const modal=document.querySelector('.inside__modal')
+const searchInput=document.querySelector('.searchInput')
+
+searchInput.addEventListener('input',(e)=>{
+    const searchText = e.target.value.toLowerCase().trim();
+
+    const filteredProducts = ProductData.filter(x => x.name.toLowerCase().trim().includes(searchText));
+    modal.innerHTML=''
+    filteredProducts.forEach(x=>{
+        modal.innerHTML+=`<a href="SingleProduct.html?${x.id}" class="first__prod"> <p>${x.name}</p>
+        <p>${x.price}</p>
+        </a>
+        `
+    })
+    
+})
+
+
+searchInput.addEventListener('focus',()=>{
+    modal.style.display="block"
+
+})
+
+
+searchInput.addEventListener('blur',()=>{
+    setTimeout(()=>{
+        modal.style.display="none"
+
+    },1000)
+})
+
+
 // 2 ci kartlar 
 
 
@@ -185,48 +217,48 @@ const ProductData2 = [
         "name": "AirPods",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FElectronics%2F29.AppleEarphones.png&w=640&q=75",
         "price": 100,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 7,
         "name": "Phone 12",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FElectronics%2F30.Nokiaandroidone.png&w=640&q=75",
         "price": 120,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 8,
         "name": "Hpc 2018",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FElectronics%2F32.iphone7.png&w=640&q=75",
         "price": 110,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 9,
         "name": "Iphone",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FElectronics%2F33.beatswirelessearphones.png&w=640&q=75",
         "price": 140,
-        "disCount":200
+        "disCount": 200
     },
     {
         "id": 10,
         "name": "Watch",
         "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FElectronics%2F34.HTC2018.png&w=640&q=75",
         "price": 160,
-        "disCount":200
+        "disCount": 200
     }
 ]
 
 async function GetHomeProducts2() {
-    const HomeSection2__container=document.getElementById("section7__row")
-    
-        // const response = await axios('yourendpoint')
-    
-        // Get product data in place
-        ProductData2.forEach(element => {
-            let product = document.createElement('div')
-            product.classList.add(`col-lg-4`)
-            product.innerHTML = `
+    const HomeSection2__container = document.getElementById("section7__row")
+
+    // const response = await axios('yourendpoint')
+
+    // Get product data in place
+    ProductData2.forEach(element => {
+        let product = document.createElement('div')
+        product.classList.add(`col-lg-4`)
+        product.innerHTML = `
             <div class="card homesection7__card" style="width: 18rem;">
             <a href="./SingleProduct.html?${element.id}">
                <div>
@@ -256,52 +288,52 @@ async function GetHomeProducts2() {
     
         
     `
-    
-            // Then send them to HTML in style
-            HomeSection2__container.append(product)
-        })
-    
+
+        // Then send them to HTML in style
+        HomeSection2__container.append(product)
+    })
+
+}
+
+GetHomeProducts2()
+
+
+
+async function AddToBasket2(id) {
+
+    // Fetch Data
+    // let response = await axios.get(`http://localhost:3000/basket`)
+
+    const productIndex = Basket.findIndex(x => x.id === id)
+
+    if (productIndex !== -1) {
+        Basket[productIndex].count += 1
+        // totalPrice
+        localStorage.setItem('basket', JSON.stringify(Basket))
     }
-    
-    GetHomeProducts2()
+    else {
+        const product = ProductData2.find(x => x.id === id)
+
+        product.count = 1
+
+        Basket = [...Basket, product]
+
+        localStorage.setItem('basket', JSON.stringify(Basket))
+    }
+
+    console.log(Basket);
+
+    // Reset basket before adding data back
+    BasketContainer.innerHTML = ''
 
 
+    Basket.forEach(element => {
 
-    async function AddToBasket2(id) {
+        // Get clicked product to wishlist
+        let item = document.createElement('div')
+        item.classList.add('wishlistProduct')
 
-        // Fetch Data
-        // let response = await axios.get(`http://localhost:3000/basket`)
-    
-        const productIndex = Basket.findIndex(x => x.id === id)
-    
-        if (productIndex !== -1) {
-            Basket[productIndex].count += 1
-            // totalPrice
-            localStorage.setItem('basket', JSON.stringify(Basket))
-        }
-        else {
-            const product = ProductData2.find(x => x.id === id)
-    
-            product.count = 1
-    
-            Basket = [...Basket, product]
-    
-            localStorage.setItem('basket', JSON.stringify(Basket))
-        }
-    
-        console.log(Basket);
-    
-        // Reset basket before adding data back
-        BasketContainer.innerHTML = ''
-    
-    
-        Basket.forEach(element => {
-    
-            // Get clicked product to wishlist
-            let item = document.createElement('div')
-            item.classList.add('wishlistProduct')
-    
-            item.innerHTML = `<img class="wishlistProductImg" src=${element.img}>    
+        item.innerHTML = `<img class="wishlistProductImg" src=${element.img}>    
                     <div class="wishlistProductTextBox">
                         <div class="headLine">
                             <h3 class="wishlistProductName">${element.name}</h3>
@@ -311,43 +343,46 @@ async function GetHomeProducts2() {
                         <br/>
                         <span class="">Total Price${element.count * element.price}</span>
                     </div>`
-    
+
+        BasketContainer.append(item)
+    });
+
+}
+
+
+function GetBasket2() {
+    try {
+        Basket = JSON.parse(localStorage.getItem('basket'))
+        console.log(Basket);
+        Basket.forEach(element => {
+
+            // Get clicked product to wishlist
+            let item = document.createElement('div')
+            item.classList.add('wishlistProduct')
+
+            // item.innerHTML = `<div class="oooo">
+            // <img class="wishlistProductImg" src=${element.image}>    
+            //         <div class="wishlistProductTextBox">
+            //             <div class="headLine">
+            //                 <h3 class="wishlistProductName">${element.name}</h3>
+            //             </div>
+            //             <p class=""><strong>Count</strong>:  ${element.count}  </p>
+            //             <p> <strong>Price</strong>: ${element.price} AZN</p>
+
+            //             <p class=""><strong>Total Price</strong>: ${element.count * element.price} AZN</p>
+            //         </div>
+            // </div>`
+
             BasketContainer.append(item)
         });
-    
+    } catch (error) {
+        localStorage.setItem('basket', JSON.stringify([]))
+        console.log(error);
     }
-    
-    
-    function GetBasket2() {
-        try {
-            Basket = JSON.parse(localStorage.getItem('basket'))
-            console.log(Basket);
-            Basket.forEach(element => {
-    
-                // Get clicked product to wishlist
-                let item = document.createElement('div')
-                item.classList.add('wishlistProduct')
-    
-                // item.innerHTML = `<div class="oooo">
-                // <img class="wishlistProductImg" src=${element.image}>    
-                //         <div class="wishlistProductTextBox">
-                //             <div class="headLine">
-                //                 <h3 class="wishlistProductName">${element.name}</h3>
-                //             </div>
-                //             <p class=""><strong>Count</strong>:  ${element.count}  </p>
-                //             <p> <strong>Price</strong>: ${element.price} AZN</p>
-                          
-                //             <p class=""><strong>Total Price</strong>: ${element.count * element.price} AZN</p>
-                //         </div>
-                // </div>`
-    
-                BasketContainer.append(item)
-            });
-        } catch (error) {
-            localStorage.setItem('basket', JSON.stringify([]))
-            console.log(error);
-        }
-    }
+}
+
+
+
 
 
 
@@ -357,55 +392,55 @@ async function GetHomeProducts2() {
 // section3 Cars api 
 
 
-    const ProductData3 = [
-        {
-            "id": 11,
-            "name": "Lord2019",
-            "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F1.Ford2019.png&w=640&q=75",
-            "price": 10000,
-            "disCount":13000
-        },
-        {
-            "id": 12,
-            "name": "Lord2017",
-            "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F2.Audi2017.png&w=640&q=75",
-            "price": 12000,
-            "disCount":13000
-        },
-        {
-            "id": 13,
-            "name": "Lord22",
-            "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F4.Porsche2018.png&w=640&q=75",
-            "price": 11000,
-            "disCount":12000
-        },
-        {
-            "id": 14,
-            "name": "Watch4",
-            "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F5.Ford2018.png&w=640&q=75",
-            "price": 14000,
-            "disCount":16000
-        },
-        {
-            "id": 15,
-            "name": "Watch",
-            "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F6.Ford2020.png&w=640&q=75",
-            "price": 16000,
-            "disCount":20000
-        }
-    ]
+const ProductData3 = [
+    {
+        "id": 11,
+        "name": "Lord2019",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F1.Ford2019.png&w=640&q=75",
+        "price": 10000,
+        "disCount": 13000
+    },
+    {
+        "id": 12,
+        "name": "Lord2017",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F2.Audi2017.png&w=640&q=75",
+        "price": 12000,
+        "disCount": 13000
+    },
+    {
+        "id": 13,
+        "name": "Lord22",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F4.Porsche2018.png&w=640&q=75",
+        "price": 11000,
+        "disCount": 12000
+    },
+    {
+        "id": 14,
+        "name": "Watch4",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F5.Ford2018.png&w=640&q=75",
+        "price": 14000,
+        "disCount": 16000
+    },
+    {
+        "id": 15,
+        "name": "Watch",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FAutomotive%2F6.Ford2020.png&w=640&q=75",
+        "price": 16000,
+        "disCount": 20000
+    }
+]
 
 
-    async function GetHomeProducts3() {
-        const Section75__row=document.getElementById("Section75__row")
-        
-            // const response = await axios('yourendpoint')
-        
-            // Get product data in place
-            ProductData3.forEach(element => {
-                let product = document.createElement('div')
-                product.classList.add(`col-lg-4`)
-                product.innerHTML = `
+async function GetHomeProducts3() {
+    const Section75__row = document.getElementById("Section75__row")
+
+    // const response = await axios('yourendpoint')
+
+    // Get product data in place
+    ProductData3.forEach(element => {
+        let product = document.createElement('div')
+        product.classList.add(`col-lg-4`)
+        product.innerHTML = `
                                 <div class="card homesection7_5__card" style="width: 18rem;">
                                 <a href="./SingleProduct.html?${element.id}">
                                 <div>
@@ -435,14 +470,14 @@ async function GetHomeProducts2() {
         
             
         `
-        
-                // Then send them to HTML in style
-                Section75__row.append(product)
-            })
-        
-        }
-        
-        GetHomeProducts3()
+
+        // Then send them to HTML in style
+        Section75__row.append(product)
+    })
+
+}
+
+GetHomeProducts3()
 
 
 
@@ -455,55 +490,55 @@ async function GetHomeProducts2() {
 
 
 
-        const ProductData5 = [
-            {
-                "id": 16,
-                "name": "Lord2019",
-                "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F7.PoliceGrayEyeglasses.png&w=640&q=75",
-                "price": 10000,
-                "disCount":13000
-            },
-            {
-                "id": 17,
-                "name": "Lord2017",
-                "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F8.RayBanMattBlack.png&w=640&q=75",
-                "price": 12000,
-                "disCount":13000
-            },
-            {
-                "id": 18,
-                "name": "Lord22",
-                "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F9.RayBanBlack.png&w=640&q=75",
-                "price": 11000,
-                "disCount":12000
-            },
-            {
-                "id": 19,
-                "name": "Watch4",
-                "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F10.RayBanOcean.png&w=640&q=75",
-                "price": 14000,
-                "disCount":16000
-            },
-            {
-                "id": 20,
-                "name": "Watch",
-                "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F11.SunglassesCollection.png&w=640&q=75",
-                "price": 16000,
-                "disCount":20000
-            }
-        ]
+const ProductData5 = [
+    {
+        "id": 16,
+        "name": "Lord2019",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F7.PoliceGrayEyeglasses.png&w=640&q=75",
+        "price": 10000,
+        "disCount": 13000
+    },
+    {
+        "id": 17,
+        "name": "Lord2017",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F8.RayBanMattBlack.png&w=640&q=75",
+        "price": 12000,
+        "disCount": 13000
+    },
+    {
+        "id": 18,
+        "name": "Lord22",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F9.RayBanBlack.png&w=640&q=75",
+        "price": 11000,
+        "disCount": 12000
+    },
+    {
+        "id": 19,
+        "name": "Watch4",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F10.RayBanOcean.png&w=640&q=75",
+        "price": 14000,
+        "disCount": 16000
+    },
+    {
+        "id": 20,
+        "name": "Watch",
+        "image": "https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FFashion%2FAccessories%2F11.SunglassesCollection.png&w=640&q=75",
+        "price": 16000,
+        "disCount": 20000
+    }
+]
 
 
-        async function GetHomeProducts5() {
-            const section9__container=document.getElementById("section9__container")
-            
-                // const response = await axios('yourendpoint')
-            
-                // Get product data in place
-                ProductData5.forEach(element => {
-                    let product = document.createElement('div')
-                    product.classList.add(`col-lg-4`)
-                    product.innerHTML = `
+async function GetHomeProducts5() {
+    const section9__container = document.getElementById("section9__container")
+
+    // const response = await axios('yourendpoint')
+
+    // Get product data in place
+    ProductData5.forEach(element => {
+        let product = document.createElement('div')
+        product.classList.add(`col-lg-4`)
+        product.innerHTML = `
                     <div class="col-lg-4 col-md-6 res__div9">
                     <div class="card homesection9__card" style="width: 18rem;">
                     <a href="./SingleProduct.html?${element.id}">
@@ -535,11 +570,11 @@ async function GetHomeProducts2() {
             
                 
             `
-            
-                    // Then send them to HTML in style
-                    section9__container.append(product)
-                })
-            
-            }
-            
-            GetHomeProducts5()
+
+        // Then send them to HTML in style
+        section9__container.append(product)
+    })
+
+}
+
+GetHomeProducts5()
